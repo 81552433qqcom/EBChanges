b331440105177eda941ceb870a97510c5cdadb98
81552433qqcom <81552433@qq.com>
2014-08-27 14:48:18 +0800

    debug

--- apps/frappe/frappe/public/js/frappe/views/doclistview.js
+++ apps/frappe/frappe/public/js/frappe/views/doclistview.js
@@ -62,7 +62,8 @@ frappe.views.DocListView = frappe.ui.Listing.extend({
 			.parent().css({"margin-top":"-15px"});
 		this.appframe = this.page.appframe;
 		var module = locals.DocType[this.doctype].module;
-
+		console.log(__(this.doctype) + __('List'));
+		
 		this.appframe.set_title(__("{0} List", __([this.doctype]));
 		this.appframe.add_module_icon(module, this.doctype, null, true);
 		this.appframe.set_title_left(function() {
