fafdcfc6ffe7076f76b131fd01e991731de3ec3d
81552433qqcom <81552433@qq.com>
2014-08-31 18:10:11 +0800

    translations.

--- apps/frappe/frappe/core/page/desktop/desktop.js
+++ apps/frappe/frappe/core/page/desktop/desktop.js
@@ -140,7 +140,7 @@ frappe.desktop.show_all_modules = function() {
 				<div class="row">\
 					<div class="col-xs-2"><a href="#%(link)s">%(app_icon)s</a></div>\
 					<div class="col-xs-10" style="padding-top: 14px;">\
-						<a href="#%(link)s">%(' + __("label") + 's</a>\
+						<a href="#%(link)s">%(' + __(label) + 's</a>\
 						<input class="pull-right" type="checkbox" data-name="%(name)s" />\
 					</div>\
 				</div>\
