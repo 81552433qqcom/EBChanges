6c4459a7e17350db0b75f07578b4d5e29d95ba99
81552433qqcom <81552433@qq.com>
2014-09-10 12:32:32 +0800

    translations. backgrounds.

--- commands/new_site.sh
+++ commands/new_site.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 cd /home/erpnext/ebchanges_staging/ebchanges
-git pull
-
-ls /home/erpnext/ebchanges_staging/ebchanges/apps/erpnext/erpnext/translations
-ls /home/erpnext/frappe-bench/apps/erpnext/erpnext/translations
+sudo git pull
 
 cp -Rf /home/erpnext/ebchanges_staging/ebchanges/apps/erpnext/erpnext/* /home/erpnext/frappe-bench/apps/erpnext/erpnext/
 cp -Rf /home/erpnext/ebchanges_staging/ebchanges/apps/frappe/frappe/* /home/erpnext/frappe-bench/apps/frappe/frappe/
@@ -13,6 +10,9 @@ chown erpnext:erpnext /home/erpnext/frappe-bench/apps/erpnext/erpnext/translatio
 
 ls /home/erpnext/frappe-bench/apps/frappe/frappe/translations/zh-cn.csv
 ls /home/erpnext/frappe-bench/apps/erpnext/erpnext/translations/zh-cn.csv
+
+#copy background images
+cp -Rf /home/erpnext/ebchanges_staging/ebchanges/sites/* /home/erpnext/frappe-bench/sites/
 
 cd /home/erpnext/frappe-bench
 bench frappe --build
